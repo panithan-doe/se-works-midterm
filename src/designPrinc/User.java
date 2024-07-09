efc5454f5991a63517adf27129e922871c79f7e4
Panithan <doedoserpc@gmail.com>
2024-07-10 00:53:54 +0700

    add @author doc comment

--- src/designPrinc/User.java
+++ src/designPrinc/User.java
@@ -1,3 +1,7 @@
+/**
+ * @author  6510450569 Panithan Champahom
+ */
+
 package designPrinc;
 
 public class User {
--- src/designPrinc/badDesign.java
+++ src/designPrinc/badDesign.java
@@ -1,3 +1,7 @@
+/**
+ * @author  6510450569 Panithan Champahom
+ */
+
 package designPrinc;
 
 import java.util.Arrays;
